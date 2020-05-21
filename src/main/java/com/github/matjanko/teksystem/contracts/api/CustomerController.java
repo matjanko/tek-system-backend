dd5c6ef6157875674b0adba960ed47fbc745447b
matjanko <mateusz.jankowski@prokonto.pl>
2020-05-21 23:15:32 +0200

    Added test post endpoint

--- src/main/java/com/github/matjanko/teksystem/contracts/api/CustomerController.java
+++ src/main/java/com/github/matjanko/teksystem/contracts/api/CustomerController.java
@@ -7,10 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +28,12 @@ public class CustomerController {
     @GetMapping("/{id}")
     @ApiOperation(value = "Get customer by id")
     public ResponseEntity<CustomerDto> getCustomers(@PathVariable Long id) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}")
+    @ApiOperation(value = "Get customer by id")
+    public ResponseEntity<CustomerDto> postCustomers(@PathVariable Long id) {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
