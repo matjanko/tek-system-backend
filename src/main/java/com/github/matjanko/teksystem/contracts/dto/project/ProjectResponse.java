cfefc5b9831ae0b12e44dd3ee104e9a65f253e4c
matjanko <mateusz.jankowski@prokonto.pl>
2020-07-04 00:40:59 +0200

    Updated ProjectResponse

--- src/main/java/com/github/matjanko/teksystem/contracts/dto/project/ProjectResponse.java
+++ src/main/java/com/github/matjanko/teksystem/contracts/dto/project/ProjectResponse.java
@@ -2,6 +2,7 @@ package com.github.matjanko.teksystem.contracts.dto.project;
 
 import com.github.matjanko.teksystem.contracts.dto.customer.CustomerResponse;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
 @Data
 public class ProjectResponse {
@@ -11,5 +12,10 @@ public class ProjectResponse {
     private Character symbol;
     private Long number;
     private CustomerResponse customer;
+
+    public String getIndex() {
+        return symbol.toString() +
+                StringUtils.leftPad(number.toString(), 4, "0") ;
+    }
 
 }
